03461eb5a5c0715cb97edec5365bf6252bd8ca6f
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-08-20 17:28:43 -0400

    add comment showing where to pass nft w, h, dpi

--- libs/artoolkitNFT.worker.js
+++ libs/artoolkitNFT.worker.js
@@ -30,6 +30,9 @@ function load (msg) {
       });
 
       ar.loadNFTMarker(msg.marker, function (nft) {
+        // TBD: if you wish to get w, h, dpi from loaded image, can read from nft here
+        //      and post nftData, getNFTData messages here to pass those to videoPlane
+        
         ar.trackNFTMarkerId(nft.id);
         console.log("loadNFTMarker -> ", nft.id);
         console.log("nftMarker struct: ", nft);
