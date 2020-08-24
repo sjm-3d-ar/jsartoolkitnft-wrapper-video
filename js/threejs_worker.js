9d7334f4faac9bb0b55bcec276dcfd2d58597163
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-08-24 14:35:52 -0400

    decrease interpolationFactor to 4 (faster tracking, more shaky)

--- js/threejs_worker.js
+++ js/threejs_worker.js
@@ -3,7 +3,7 @@ function isMobile () {
 }
 
 // NOTE: the original value of this was 24, which was very smooth, but slower to track
-var interpolationFactor = 12;
+var interpolationFactor = 4;
 
 var trackedMatrix = {
   // for interpolation
